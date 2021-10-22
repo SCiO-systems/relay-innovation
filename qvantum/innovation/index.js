8d4ba078f3eeab90b64ca15c2a76e1f3713412f0
sotkonstantinidis <sotiris@scio.systems>
2021-10-22 14:27:02 +0300

    Innovation
    - Get Innovation Document from ES Call

--- qvantum/innovation/index.js
+++ qvantum/innovation/index.js
@@ -25,7 +25,32 @@ const fetchRTBResults = async(data,access_token)=>{
     let dataRequest = JSON.stringify(data);
     let config = {
         method: 'post',
-        url: process.env.AGRONOMY_API + '/api/rtb',
+        url: process.env.AGRONOMY_API + '/api/rtb-search',
+        headers: {
+            'Authorization': `Bearer ` + access_token,
+            'Content-Type': 'application/json'
+        },
+        data: dataRequest
+    };
+
+    return axios(config)
+        .then(function (response) {
+            return response.data;
+        })
+        .catch(function (error) {
+            Logger.error(JSON.stringify(error));
+            Logger.error("AXIOS CONFIG: "+JSON.stringify(config));
+            return error;
+        });
+
+}
+
+
+const fetchDocument = async(data,access_token)=>{
+    let dataRequest = JSON.stringify(data);
+    let config = {
+        method: 'post',
+        url: process.env.AGRONOMY_API + '/api/retrievedocument',
         headers: {
             'Authorization': `Bearer ` + access_token,
             'Content-Type': 'application/json'
@@ -50,6 +75,14 @@ router.post("/search",async(req,res)=>{
     const token = await generateToken();
     const query = req.body;
     const data = await fetchRTBResults(query,token.response.access_token);
+    res.json(data);
+})
+
+router.post("/document",async(req,res)=>{
+    Logger.info("Get Document "+JSON.stringify(req.body));
+    const token = await generateToken();
+    const query = req.body;
+    const data = await fetchDocument(query,token.response.access_token);
     res.json(data);
 })
 
